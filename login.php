ec8e9109e9c9c1cb0aa901e68c1e4119826e03cb
Andrea Pulcini <andripulc@hotmail.it>
2021-12-03 16:43:02 +0100

    my-account page

--- login.php
+++ login.php
@@ -5,18 +5,29 @@
   
   session_start();
 
+ /* if(empty($_POST["username"]) && empty($_POST["password"]))                           DA IMPLEMENTARE
+      {  
+           echo '<script>alert("Both Fields are required")</script>';  
+           header("Location: login-register.php");
+      }  
+else{ */
+
   if(isset($_POST['login_user'])) {  
     $username=$_POST['username'];  
-    $password=md5($_POST['password']);  
+    $password=($_POST['password']);
+    $password=md5($password); 
   
     $check_user="select * from user WHERE username='$username'AND password='$password' ";  
-   
-    $run=mysqli_query($mysqli,$check_user);  
 
+    $run=mysqli_query($mysqli,$check_user);  
+  }
     while ($data = $run->fetch_assoc()) {
       $id_user = $data['id_user'];
-    }
+      $username = $data['username'];
+      $password = $data['password'];
 
+    }
+    
     $check_group="select group_type from group_user where user='$id_user'";
     $run1=mysqli_query($mysqli,$check_group);
 
@@ -24,20 +35,24 @@
       $group_type = $data['group_type'];
       
     }
-  
+
     if(mysqli_num_rows($run)) {  
 
         if($group_type == 1){
           header("Location: index-admin.php");
         }else{
-          header("Location: index.php");
+          header("Location: my-account.php");
         }
     }else{  
 
       $main = new Template("dtml/login.html");
       $main->setContent("message", "Username or password are incorrect!");
     }  
-}  
+  
+$_SESSION['username']= $username;
+
+
+
 
 $main->close();
 
