40d7d5da9aaa57d11642ea37cfd248a70535bab2
Lorenc Markja <lorencmarkja97@gmail.com>
2021-12-12 19:00:42 +0100

    shopfullwidth paginazione

--- shop-fullwidth.php
+++ shop-fullwidth.php
@@ -6,12 +6,30 @@
     $main = new Template("dtml/shop-fullwidth.html");
     require "include/isLogged.inc.php";
 
+// Current Page Number
+    if (isset($_GET['page-number']) && $_GET['page-number']!="") {
+        $page_no = $_GET['page-number'];
+        } else {
+            $page_no = 1;
+            }
 
-    $product = $mysqli->query("SELECT * FROM product_info ORDER BY id_product;");
-    $count = 0;
+    $total_records_per_page = 12;
 
+//OFFSET Value and SET other Variables
+    $offset = ($page_no-1) * $total_records_per_page;
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
+    $adjacents = "2";
+
+//Total Number of Pages for Pagination
+    $records = mysqli_query($mysqli, "SELECT COUNT(*) AS total_records FROM `product_info`");
+        $total_records = mysqli_fetch_array($records);
+        $total_records = $total_records['total_records'];
+        $total_no_of_pages = ceil($total_records / $total_records_per_page);
+        $second_last = $total_no_of_pages - 1;
+
+    $product = $mysqli->query("SELECT * FROM product_info ORDER BY id_product LIMIT $offset, $total_records_per_page;");
     while ($data = $product->fetch_assoc()) {
-        $count++;
         $id=$data['id_product'];
         $main->setContent("name", $data['name']);            
         $main->setContent("price", $data['price']);
@@ -21,7 +39,22 @@
         $main->setContent("info_sort", "<li data-id='$id' data-price='$price' class='items'>");
     }
 
-    $main->setContent("count", $count);
+    $contatore = $total_no_of_pages; 
+    $contatore_pagine = 2;
+    while($contatore > 1 ){
+        $main->setContent("page_number", "<a href='shop-fullwidth.php?page-number=$contatore_pagine'> $contatore_pagine </a>");
+        $contatore_pagine = $contatore_pagine + 1;
+        $contatore = $contatore -1;
 
+    }
+
+    //sezione showing results
+    //nella prima pagina deve mostrare                         1          -    $total_records_per_page of 18 $recordsTotal                          1 - 12    of 18
+    //nella seconda pagina deve mostrare ($total_records_per_page)+1      -    ($total_records_per_page * 2)                                        13 - 24
+    //nella terza pagina deve mostrare   ($total_records_per_page * 2)+1  -    ($total_records_per_page * 3)                                        25 - 36 
+    //nella quarta pagina deve mostrare  ($total_records_per_page * 3)+1  -    ($total_records_per_page * 4)                                        37 - 49   
+
+
+    $main->setContent("recordsTotal", $total_records);
     $main->close();
 ?>
