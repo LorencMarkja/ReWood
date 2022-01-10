09f73376ca69a3c2b8f2681805621820656046ef
Emanuela Gonzales <emanuela.gonzales@student.univaq.it>
2022-01-10 15:15:19 +0100

    fix additem button product page

--- addItemCart.php
+++ addItemCart.php
@@ -11,21 +11,26 @@
     //$main = new Template("dtml/shop-fullwidth.html");
 
     if (isset($_GET['quantity-value'])){
-        $quantity = $_GET['quantity-value'];
-        $name =  $_GET['name'];
-        $checkProduct = "select * FROM cart_product where product='$id_product' AND cart='$id_cart'";
-        $run=mysqli_query($mysqli,$checkProduct);
-        $rowCount= mysqli_num_rows($run);
 
-        if($rowCount == 0){
-            $addItemCart = "INSERT INTO cart_product (cart, product, quantity) VALUES ('$id_cart', '$id_product', '$quantity')";
-            $run2=mysqli_query($mysqli,$addItemCart);
+        if(isset($_SESSION['username'])){
+            $quantity = $_GET['quantity-value'];
+            $name =  $_GET['name'];
+            $checkProduct = "select * FROM cart_product where product='$id_product' AND cart='$id_cart'";
+            $run=mysqli_query($mysqli,$checkProduct);
+            $rowCount= mysqli_num_rows($run);
+    
+            if($rowCount == 0){
+                $addItemCart = "INSERT INTO cart_product (cart, product, quantity) VALUES ('$id_cart', '$id_product', '$quantity')";
+                $run2=mysqli_query($mysqli,$addItemCart);
+            }else{
+                $updateCart = "UPDATE cart_product SET quantity = quantity + $quantity  WHERE product='$id_product' AND cart='$id_cart'";
+                $run3=mysqli_query($mysqli,$updateCart);
+            }
+            header("Location: product-page.php?name=$name");
         }else{
-            $updateCart = "UPDATE cart_product SET quantity = quantity + $quantity  WHERE product='$id_product' AND cart='$id_cart'";
-            $run3=mysqli_query($mysqli,$updateCart);
+            header("Location: login-register.php");
         }
-
-        header("Location: product-page.php?name=$name");
+        
     }else{
         $checkProduct = "select * FROM cart_product where product='$id_product' AND cart='$id_cart'";
         $run4=mysqli_query($mysqli,$checkProduct);
