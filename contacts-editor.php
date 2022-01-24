207ba89bcced371ebf3893437cf54619cc64d75a
Emanuela Gonzales <emanuela.gonzales@student.univaq.it>
2022-01-24 13:34:05 +0100

    back office
    Added script and new html pages to the back office

---
+++ contacts-editor.php
@@ -0,0 +1,10 @@
+<?php
+    require "include/connection_db.inc.php";
+    require "include/template2.inc.php";
+    
+    $main = new Template("dtml/contacts-editor.html");
+
+
+    $main->close();
+
+?>
